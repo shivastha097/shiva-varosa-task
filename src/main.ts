f6f5858043e22e9daf5e931ae47a9d290264d50f
Shiva Shrestha <shivastha097@gmail.com>
2022-11-04 11:11:41 +0545

    packages installation and run express server

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.log("Setting up!");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const BASE_URL = process.env.NODE_ENV === "dev" ? process.env.DEV_BASE_URL : process.env.PROD_BASE_URL;
+
+const bootstrap = async () => {
+    const app = express();
+
+    app.get("/status", (req, res) => {
+        return res.json({ status: "OK" });
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server running on ${BASE_URL}:${PORT}`);
+    });
+};
+bootstrap();
